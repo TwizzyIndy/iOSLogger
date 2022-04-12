5b624825f17a2af8b83977f378c111ac56ee58ef
TwizzyIndy <twizzy.indy@gmail.com>
2022-04-12 13:58:34 +0630

    Initial Commit

---
+++ iOSLogger/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOSLogger
+//
+//  Created by Aung Khant M. on 12/04/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
