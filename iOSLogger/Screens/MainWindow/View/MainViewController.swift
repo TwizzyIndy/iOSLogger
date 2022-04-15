af83a0c547f4b7ff48f59f3a32782c1b35dff631
TwizzyIndy <twizzy.indy@gmail.com>
2022-04-15 14:54:06 +0630

    display logs on tableview

--- iOSLogger/Screens/MainWindow/View/MainViewController.swift
+++ iOSLogger/Screens/MainWindow/View/MainViewController.swift
@@ -8,11 +8,65 @@
 import Cocoa
 
 class MainViewController: NSViewController {
-
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var tableView: NSTableView!
+    
+    //MARK: - Properties
+    var logMessageList : [LogMessageModel]?
+    var consoleHelper : LogConsoleHelper!
+    
+    //MARK: - VC LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupViews()
+
+        consoleHelper = LogConsoleHelper(onStdOut: { [unowned tableView] text in
+            
+            let line = LogLine(logMessage: text)
+            if let msg = line.messageModel {
+                self.logMessageList?.append(msg)
+                
+                // we should have to re-select the row
+                let selectedRow = tableView?.selectedRowIndexes
+                
+                DispatchQueue.main.async {
+                    tableView?.reloadData()
+                    
+                    if let x = selectedRow {
+                        tableView?.selectRowIndexes(x, byExtendingSelection: false)
+                    }
+                }
+            }
+        }, onStdErr: { [unowned tableView] text in
+            
+            let line = LogLine(logMessage: text)
+            if let msg = line.messageModel {
+                
+                self.logMessageList?.append(msg)
+                
+                // we should have to re-select the row
+                let selectedRow = tableView?.selectedRowIndexes
+                
+                DispatchQueue.main.async {
+                    tableView?.reloadData()
+                    
+                    if let x = selectedRow {
+                        tableView?.selectRowIndexes(x, byExtendingSelection: false)
+                    }
+                }
+            }
+        })
+        
+    }
+    
+    private func setupViews()
+    {
+        self.logMessageList = [LogMessageModel]()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self        
     }
 
     override var representedObject: Any? {
@@ -21,6 +75,47 @@ class MainViewController: NSViewController {
         }
     }
 
-
+    deinit {
+        
+    }
 }
 
+//MARK: - NSTableView extensions
+extension MainViewController: NSTableViewDelegate, NSTableViewDataSource {
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        self.logMessageList?.count ?? 0
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        
+        var text = ""
+        var cellIdentifier = ""
+        
+        guard let model = self.logMessageList?[row] else {
+            return nil
+        }
+        
+        if tableColumn == tableView.tableColumns[0] {
+            text = "\(model.time)"
+            cellIdentifier = "timeCell"
+        } else if tableColumn == tableView.tableColumns[1] {
+            text = "\(model.processName)"
+            cellIdentifier = "processCell"
+        } else if tableColumn == tableView.tableColumns[2] {
+            text = "\(model.messageDetail)"
+            cellIdentifier = "descriptionCell"
+        } else if tableColumn == tableView.tableColumns[3] {
+            text = "\(model.messageType)"
+            cellIdentifier = "typeCell"
+        }
+        
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+            cell.textField?.stringValue = text
+            
+            return cell
+        }
+        
+        return nil
+    }
+}
